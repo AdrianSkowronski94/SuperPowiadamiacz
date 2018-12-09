ea7ca87d92f7d83f8ebc53e4c90501c797225cc8
Joanna Waligóra <jwaligora96@gmail.com>
2018-12-09 10:42:54 +0100

    Show reminders, delete selected

---
+++ app/src/main/java/xyz/reminder/superreminder/adapters/RemindersAdapter.java
@@ -0,0 +1,77 @@
+package xyz.reminder.superreminder.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import xyz.reminder.superreminder.R;
+import xyz.reminder.superreminder.database.reminder.Reminder;
+
+/**
+ * Created by Joanna on 09.12.2018.
+ */
+
+public class RemindersAdapter extends RecyclerView.Adapter<RemindersAdapter.RemindersViewHolder> {
+
+    private List<Reminder> remindersList;
+    private IRecyclerViewClickListener listener;
+
+    public class RemindersViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView reminderName, reminderTime, reminderDate;
+
+        public RemindersViewHolder(View itemView) {
+            super(itemView);
+
+            reminderName = itemView.findViewById(R.id.reminderName);
+            reminderTime = itemView.findViewById(R.id.reminderTime);
+            reminderDate = itemView.findViewById(R.id.reminderDate);
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    listener.onLongClicked(getAdapterPosition());
+                    return false;
+                }
+            });
+
+        }
+    }
+
+    public RemindersAdapter(List<Reminder> remindersList, IRecyclerViewClickListener listener)
+    {
+        this.remindersList = remindersList;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public RemindersViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.reminder_card, viewGroup, false);
+        return new RemindersViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RemindersViewHolder remindersViewHolder, int i) {
+        Reminder reminder = remindersList.get(i);
+        remindersViewHolder.reminderName.setText(reminder.getName());
+        Timestamp ts = reminder.getTime();
+        remindersViewHolder.reminderTime.setText(String.valueOf(ts.getHours()) + ":" + String.valueOf(ts.getMinutes()));
+        remindersViewHolder.reminderDate.setText(String.valueOf(ts.getDay()) + "/" +
+                                                    String.valueOf(ts.getMonth())+ "/" +
+                                                    String.valueOf(ts.getYear()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return remindersList.size();
+    }
+
+}
--- app/src/main/java/xyz/reminder/superreminder/fragments/ListFragment.java
+++ app/src/main/java/xyz/reminder/superreminder/fragments/ListFragment.java
@@ -1,20 +1,33 @@
 package xyz.reminder.superreminder.fragments;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import xyz.reminder.superreminder.R;
-import xyz.reminder.superreminder.activities.MainActivity;
+import xyz.reminder.superreminder.adapters.IRecyclerViewClickListener;
+import xyz.reminder.superreminder.adapters.RemindersAdapter;
 import xyz.reminder.superreminder.controllers.StyleController;
+import xyz.reminder.superreminder.database.ReminderDbManager;
+import xyz.reminder.superreminder.database.reminder.Reminder;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 public class ListFragment extends StyleFragment {
+
+    private RemindersAdapter adapter;
+    private List<Reminder> remindersList;
+    private RecyclerView recyclerView;
+    private ReminderDbManager reminderDb;
+    private IRecyclerViewClickListener listener;
 
     @Nullable
     @Override
@@ -22,6 +35,55 @@ public class ListFragment extends StyleFragment {
         super.onCreateView(inflater, container, savedInstanceState);
 
         return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = view.findViewById(R.id.reminderRecyclerView);
+        reminderDb = new ReminderDbManager(getContext());
+        remindersList = reminderDb.getReminderDAO().selectAll();
+        listener = this::showActionsDialog;
+
+        adapter = new RemindersAdapter(remindersList, listener);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        adapter.notifyDataSetChanged();
+    }
+
+    private void showActionsDialog(final int position) {
+
+        DialogInterface.OnClickListener dialogClickListener = (dialog, which) -> {
+            switch (which){
+                case DialogInterface.BUTTON_POSITIVE:
+                    deleteReminder(position);
+                    break;
+
+                case DialogInterface.BUTTON_NEGATIVE:
+                    dialog.cancel();
+                    break;
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setMessage(getContext().getString(R.string.delete)).setPositiveButton(getContext().getString(R.string.yes), dialogClickListener)
+                .setNegativeButton(getContext().getString(R.string.cancel), dialogClickListener).show();
+
+    }
+
+    private void deleteReminder(int position) {
+        reminderDb.getReminderDAO().delete(remindersList.get(position));
+        remindersList.remove(position);
+        adapter.notifyItemRemoved(position);
     }
 
     protected void fillColorMaps() {
@@ -32,3 +94,4 @@ public class ListFragment extends StyleFragment {
         textColorMap.put(R.id.list_text, StyleController.TEXT_PRIMARY);
     }
 }
+
